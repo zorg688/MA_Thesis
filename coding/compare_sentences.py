b21ea135f9b7cd12ee97126afb5f8a7caf237cb3
Daniel <danielpietschke@gmx.de>
2025-05-17 16:28:54 +0300

    added new tool for comparing sentences, renamed older files to correspond to naming convention

---
+++ coding/compare_sentences.py
@@ -0,0 +1,68 @@
+
+def read_sentences(is_baseline, model_name, model_size, sentence_index):
+
+    with open("data/4_Spanglish_Hinglish/mt_spanglisheng/test.txt", mode="r", encoding="UTF-8") as file:
+        lines = file.readlines()
+        sentence = lines[int(sentence_index)-1]
+    print(f"This is the original sentence {sentence_index}:\n{sentence}\n")
+
+    if not is_baseline:
+        with open(f"../Predictions_for_submission/{model_name}_pred.en", mode="r", encoding="UTF-8") as file:
+            lines = file.readlines()
+            sentence = lines[int(sentence_index)-1]
+        print(f"This is the teacher prediction for sentence{sentence_index}:\n{sentence}\n")
+
+    if is_baseline:
+        for index in range(5):
+            with open(f"../Predictions_for_submission/Student_Predictions/{model_name}/{model_name}_{model_size}_model{index+1}.en", mode="r", encoding="UTF-8") as file:
+                lines = file.readlines()
+                print(f"Model {index+1} prediction for sentence {sentence_index}:\n{lines[int(sentence_index)-1]}\n")
+    else: 
+        for index in range(5):
+            with open(f"../Predictions_for_submission/Student_Predictions/{model_name}/{model_name}_{model_size}_student{index+1}.en", mode="r", encoding="UTF-8") as file:
+                lines = file.readlines()
+                print(f"Model {index+1} prediction for sentence {sentence_index}:\n{lines[int(sentence_index)-1]}\n")
+
+
+def check_baseline(model_index):
+    return model_index in ["1", "2"]
+
+
+
+if __name__=="__main__":
+    model_sets = {"1": "baseline2021", 
+                  "2": "baseline2023", 
+                  "3": "cat+oci+spa-eng", 
+                  "4": "mul-mul", 
+                  "5": "defps-mul"}
+    model_size = {"1": "transformer", "2": "transformer-tiny"}
+
+    keep_going = True
+
+    while keep_going:
+        print("These are the available models: ")
+        for item in list(model_sets.items()):
+            print(f"Press {item[0]} for model set {item[-1]}")
+
+        choice_model = input("\nEnter your choice here: ")
+        
+        is_baseline = check_baseline(choice_model)
+
+
+        print("\nThese are the possible model sizes: ")
+        for item in list(model_size.items()):
+            print(f"Press {item[0]} for model set {item[-1]}")
+        choice_size = input("\nEnter your choice: ")
+
+        print("\nThank you! Please choose a sentence index now")
+        sentence_index = input("Enter your index here: ")
+
+        read_sentences(is_baseline, model_sets[choice_model], model_size[choice_size], sentence_index)
+
+        input("\nWaiting for confirmation...")
+
+        decision = input("Would you like to check another set or another sentence? \nPress 'y'to keep going, or press 'n' to close the program" )
+
+        if decision == "n":
+            keep_going = False
+
